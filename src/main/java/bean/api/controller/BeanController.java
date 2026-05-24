1fe39f6f8127f4fef3e3d037cc1bf17c52873f7b
Alvien Salvador <96222765+Biboy-al@users.noreply.github.com>
2026-05-24 15:33:20 +1200

    9 add deploy job (#10)
    * 9 add deploy job
    
    * 9 add deploy job
    
    * Comment out deploy job, and apply spot less

--- src/main/java/bean/api/controller/BeanController.java
+++ src/main/java/bean/api/controller/BeanController.java
@@ -4,13 +4,10 @@ import bean.api.dto.BeanDTO;
 import bean.api.dto.CreateBeanRequest;
 import bean.api.service.AddBeanService;
 import bean.api.service.BeanQueryService;
-
 import java.util.List;
-
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/bean")
@@ -31,9 +28,9 @@ public class BeanController {
 
   @GetMapping
   public List<BeanDTO> getAllBean(@RequestParam String param) {
-      return this.beanQueryService.findAll();
+    return this.beanQueryService.findAll();
   }
-  
+
   @GetMapping("/{id}")
   public BeanDTO getBeanById(@PathVariable("id") String id) {
 
--- src/main/java/bean/api/service/AddBeanServiceImpl.java
+++ src/main/java/bean/api/service/AddBeanServiceImpl.java
@@ -4,25 +4,25 @@ import bean.api.domain.Bean;
 import bean.api.dto.BeanDTO;
 import bean.api.dto.CreateBeanRequest;
 import bean.api.repos.BeanRepository;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
+import org.springframework.stereotype.Service;
 
 @Service
 public class AddBeanServiceImpl implements AddBeanService {
 
-    private final BeanRepository beanRepository;
+  private final BeanRepository beanRepository;
 
-    public AddBeanServiceImpl(BeanRepository beanRepository) {
-        this.beanRepository = beanRepository;
-    }
+  public AddBeanServiceImpl(BeanRepository beanRepository) {
+    this.beanRepository = beanRepository;
+  }
 
-    public BeanDTO createBean(CreateBeanRequest createBeanRequest) {
+  public BeanDTO createBean(CreateBeanRequest createBeanRequest) {
 
-        Bean bean = new Bean("Supreme", "Supreme is good");
+    Bean bean = new Bean("Supreme", "Supreme is good");
 
-        Bean savedBean = beanRepository.save(bean);
+    Bean savedBean = beanRepository.save(bean);
 
-        return new BeanDTO(savedBean.getId(), savedBean.getName(), savedBean.getDescription(), List.of());
-    }
+    return new BeanDTO(
+        savedBean.getId(), savedBean.getName(), savedBean.getDescription(), List.of());
+  }
 }
